e651f0bce5cc92181bd9b375da5a993a51bd2f5d
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-08-25 15:32:10 +0700

    .

--- Node/Importing Core.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-fs.writeFileSync('notes.txt','I Live in Philadelphia')
-jhjhjhjh\
-\
--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,5 @@
-console.log('Hello Node.js')
+//console.log('Hello Node.js')
+
+const fs = require('fs') // membuat file system
+fs.writeFileSync('notes.txt', 'I Live in Philadelphia')
+
