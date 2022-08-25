6f80753d0fa63859bfd39a522e3e9ce19b10b429
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-08-25 16:14:41 +0700

    SECTION 3 LESSON 4

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const checkUtils = require('./src/utils.js')
-checkUtils()
+//const checkUtils = require('./src/utils.js')
+//checkUtils()
+
+/* const command = process.argv[2]
+if (command === 'add') {
+ console.log('Adding note!')
+} else if (command === 'remove') {
+ console.log('Removing note!')
+} */
+
+const yargs = require('yargs')
+yargs.version('1.1.0')
+yargs.command({
+ command: 'add',
+ describe: 'Add a new note',
+ handler: function () {
+ console.log('Adding a new note!')
+ }
+})
+console.log(yargs.argv)
+
