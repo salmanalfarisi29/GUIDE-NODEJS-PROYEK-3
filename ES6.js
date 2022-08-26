0ca50a9856eec7a47e8c654c1fe7059cb6d17098
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-08-27 02:14:05 +0700

    binding

--- ES6.js
+++ ES6.js
@@ -4,7 +4,20 @@
 
 console.log(square(2))*/
 
-//shorhand syntax
-
+/*shorhand syntax
 const squareAlt = (x) => x * x
-console.log(squareAlt(2))
+console.log(squareAlt(2))*/
+
+//This Binding
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name)
+    
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name)
+    })
+    }
+   }
+   event.printGuestList()
