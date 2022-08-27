22978cdaba5a16cd1b0f556cab3837d9f7e28a77
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-08-27 23:14:17 +0700

    Asynchronous Basics

--- app.js
+++ app.js
@@ -33,3 +33,10 @@ yargs.command({
 })
 console.log(yargs.argv)
 
+console.log('Starting')
+//Wait 2 seconds before running the function
+setTimeout(() => {
+    console.log('2 Second Timer')
+   }, 2000)
+   console.log('Stopping')
+
